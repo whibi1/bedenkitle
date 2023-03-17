f73260560b79819a9e49860d74cd0ec6e414650d
Whibi <whibi38@gmail.com>
2023-03-18 00:11:04 +0300

    bedenkitle
    Basit beden kitle indeksi programı

---
+++ Bedenkitle/beden kitle indeksi.py
@@ -0,0 +1,5 @@
+print("Beden kitle indeksi programına hoşgeldiniz.")
+boy= float(input("lütfen boyunuzu m cinsinden giriniz\n"))
+kilo= int(input("Lütfen kilonuzu giriniz\n"))
+
+print("Beden Kitle indexiniz:{}'dir".format(round(kilo/(boy*boy),2)))
